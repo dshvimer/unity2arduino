1098b978c8374fe16a0620bdc4bd42ec7f5459d6
David Shvimer <david@Davids-MacBook-Pro.local>
2019-02-04 18:46:31 -0500

    init repo

---
+++ Assets/Arduino.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Arduino : MonoBehaviour {
+
+    const string PORTNAME = "/dev/tty.usbmodem14511";
+    SerialPort stream;
+    // Use this for initialization
+    void Start()
+    {
+        stream = new SerialPort(PORTNAME, 57600, Parity.None, 8, StopBits.One);
+        stream.Open();
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        print("yo");
+        stream.WriteLine("yo");
+        stream.BaseStream.Flush();
+    }
+}
